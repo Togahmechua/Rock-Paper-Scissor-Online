f12a019f413322a5deeae2fafad1045df0516997
Togahmechua <136262133+Togahmechua@users.noreply.github.com>
2025-02-05 22:54:45 +0700

    Fibonacy

--- Assets/_Game/Script/Data Structure and Algorithm/Other/Recursion.cs
+++ Assets/_Game/Script/Data Structure and Algorithm/Other/Recursion.cs
@@ -1,8 +1,12 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Recursion : MonoBehaviour
 {
     [SerializeField] private int n = 5;
+
+    //private Dictionary<int, int> memo = new Dictionary<int, int>();
+    private int[] memoArr;
 
     private void Start()
     {
@@ -10,6 +14,15 @@ public class Recursion : MonoBehaviour
         {
             Debug.Log("N phai lon hon 2");
         }
+
+        memoArr = new int[n + 1];
+
+        //Khởi tạo giá trị chưa tính
+        for (int i = 0; i <= n; i++)
+        {
+            memoArr[i] = -1;
+        }
+
 
         try
         {
@@ -25,15 +38,52 @@ public class Recursion : MonoBehaviour
 
 
     #region Fibonacci - Brute force
+    /*    private int F(int x)
+        {
+            if (x == 0)
+                return 0;
+
+            if (x == 1)
+                return 1;
+
+            return F(x - 1) + F(x - 2);
+        }*/
+    #endregion
+
+    #region Fibonacci - Brute force
+
+    //Cách 1 sử dụng Dictionary
+    /* private int F(int x)
+     {
+         if (x == 0)
+             return 0;
+
+         if (x == 1)
+             return 1;
+
+         // Kiểm tra nếu đã tính trước đó
+         if (memo.ContainsKey(x))
+             return memo[x];
+
+         memo[x] = F(x - 1) + F(x - 2);
+             return memo[x];
+     }*/
+
+    //Cách 2 sử dụng Array
     private int F(int x)
     {
         if (x == 0)
             return 0;
 
-        if (x == 1) 
+        if (x == 1)
             return 1;
 
-        return F(x - 1) + F(x - 2); 
+        // Kiểm tra nếu đã tính trước đó
+        if (memoArr[x] != -1)
+            return memoArr[x];
+
+        memoArr[x] = F(x - 1) + F(x - 2);
+        return memoArr[x];
     }
     #endregion
 }
